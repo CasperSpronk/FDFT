ced12fbacb9a73f5650a753904800fb161d2aa66
CSpronk <casperspronk@live.nl>
2018-11-30 12:46:32 +0100

    Update homework_3.m

--- homework_3.m
+++ homework_3.m
@@ -4,15 +4,20 @@
 % Gerardo Moyers
 % Casper Spronk
 %% Deterministic Limit Checks
-mean_change = 0.1;
+limit = 0.5;
+mean_change = 2;
 x = rand(1,1000) - 0.5;
 x = [x mean_change+rand(1,1000)];
   
 
 
-out = deterministic_limit(x,0.5);
+out = deterministic_limit(x,limit);
 plot(x)
 hold on
 plot(out)
+hold on
+plot(limit*ones(1,length(x)))
+hold on
+plot(-limit*ones(1,length(x)))
 
 
