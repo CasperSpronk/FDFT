1441d6712cf8ee26166b91b114e4f041012af3e5
Jortub <41167251+Jortub@users.noreply.github.com>
2018-11-30 14:39:30 +0100

    added prob method

--- homework_3.m
+++ homework_3.m
@@ -3,12 +3,15 @@
 % Jorge Bonekamp
 % Gerardo Moyers
 % Casper Spronk
+clear all
+close all
 %% Deterministic Limit Checks
-limit = 0.5;
-mean_change = 0.1;
-x = rand(1,1000) - 0.5;
-x = [x mean_change+rand(1,1000)];
-k = 10; 
+limit = 0.25;
+mean_change = 2;
+Noise               = wgn(1,1000, 1);
+x = Noise;
+x = [x mean_change+Noise];
+k = 100; 
 
 
 out = deterministic_limit(x,limit);
@@ -20,7 +23,7 @@ plot(limit*ones(1,length(x)))
 hold on
 plot(-limit*ones(1,length(x)))
 
-
+%% Windowed Deterministic Limit Check
 [new_out, average] = new_deterministic_limit(x,limit,k);
 figure
 % plot(x)
@@ -33,3 +36,24 @@ plot(-limit*ones(1,length(x)))
 hold on
 plot(average)
 
+
+%% Probablistic test
+meanEst = zeros(1,length(x));
+for i = 2:length(x)
+    meanEst(i) = meanEst(i-1) + 1/i * (x(i) - meanEst(i-1)); 
+end
+
+variancesquared = zeros(1,length(x));
+
+for i = 3:length(x)
+    disp(i)
+    disp(variancesquared(i))
+    disp(x(i))
+    disp(meanEst(i))
+    variancesquared = (i-2)/(i-1) * variancesquared(i-1) + (x(i) - meanEst(i-1))^2;
+end
+
+figure
+plot(x)
+hold on
+plot(meanEst)
