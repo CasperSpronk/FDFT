844fa24dbed8fa1fa34dfac4c9e49bbd158bb377
CSpronk <casperspronk@live.nl>
2019-01-07 14:07:07 +0100

    .

--- in_class_exercises/FD_files_ferrari/ProblemData.m
+++ in_class_exercises/FD_files_ferrari/ProblemData.m
@@ -3,6 +3,7 @@ clear all
 % Sampling time, stop time
 Ts = 0.01;
 Tstop = 1000;
+Tdelay = 500;
 
 % Real, physical parameters
 
@@ -22,8 +23,8 @@ pumpACMagRelative = [0.1 0.25];
 
 %Faults
 
-faultTime = [1e6 400 1e6];
-faultMag = [0 0.01 0];
+faultTime = [1e6 1e6 400];
+faultMag = [0 0 1];
 
 % Measurement noise
 
@@ -47,20 +48,78 @@ pipeCoeffN = makeParUncertain(pipeCoeff,uncRelative);
 
 Lambda = diag([0.91 0.91 0.91]);
 
+time_delay = 0.5;
+%%
 
-simOut = sim("centralized_FD_basic_01.slx");
+simOut = sim("centralized_FD_basic_01_2017b.slx");
+%% check if LFD observer without delay is the same as FD observer
+clc
+max_acceptable_deviation = 0.001;
+max_deviation_delay = 0;
+max_deviation_no_delay = 0;
+flag = 0;
+for i = 1:length(zero_check_no_delay)
+    if zero_check_no_delay(i,1) ~= 0
+        flag = 1;
+        if max_deviation_delay <= abs(zero_check_no_delay(i,1))
+            max_deviation_no_delay = abs(zero_check_no_delay(i,1));
+        end
+    end
+    if zero_check_no_delay(i,2) ~= 0
+        flag = 1;
+        if max_deviation_delay <= abs(zero_check_no_delay(i,2))
+            max_deviation_no_delay = abs(zero_check_no_delay(i,2));
+        end
+    end
+    if zero_check_no_delay(i,3) ~= 0
+        flag = 1;
+        if max_deviation_delay <= abs(zero_check_no_delay(i,3))
+            max_deviation_no_delay = abs(zero_check_no_delay(i,3));
+        end
+    end
+end
+if flag == 0
+    disp("LFD observer without time delay is working correctly")
+else
+    disp("fault in LFD observer without time delay")
+    disp("max deviation = " + max_deviation_no_delay + " liter")
+end
+
+flag = 0;
+for i = 1:length(zero_check_delay)
+    if abs(zero_check_delay(i,1)) >= max_acceptable_deviation
+        flag = 1;
+        if max_deviation_delay <= abs(zero_check_delay(i,1))
+            max_deviation_delay = abs(zero_check_delay(i,1));
+        end
+    end
+    if abs(zero_check_delay(i,2)) >= max_acceptable_deviation
+        flag = 1;
+        if max_deviation_delay <= abs(zero_check_delay(i,2))
+            max_deviation_delay = abs(zero_check_delay(i,2));
+        end
+    end
+    if abs(zero_check_delay(i,3)) >= max_acceptable_deviation
+        flag = 1;
+        if max_deviation_delay <= abs(zero_check_delay(i,3))
+            max_deviation_delay = abs(zero_check_delay(i,3));
+        end
+    end
+end
+if flag == 0
+    disp("LFD observer with time delay is acceptable")
+else
+    disp("fault in LFD observer with time delay")
+    disp("max deviation = " + max_deviation_delay + " liter")
+end
 %% plots
-figure("name","real system vs observer")
-hold on
-grid on
-plot(y.time,y.data)
+close all
+figure("name","check if FD observer is the same as LFD observer")
+subplot(1,2,1)
 plot(y_hat.time,y_hat.data)
-legend("Tank 1 true level","Tank 2 true level","Tank 3 true level","Tank 1 observer level","Tank 2 observer level","Tank 3 observer level")
-
-
-figure("name","real system vs LFD without delay")
-hold on
+legend("Tank 1 level","Tank 2 level","Tank 3 level")
 grid on
-plot(y.time,y.data)
-plot(y_hat_LFD_no_delays_minus_y_hat.time,y_hat_LFD_no_delays_minus_y_hat.data)
-legend("Tank 1 true level","Tank 2 true level","Tank 3 true level","Tank 1 observer level","Tank 2 observer level","Tank 3 observer level")
+subplot(1,2,2)
+plot(y_hat_LFD_delayed.time,y_hat_LFD_delayed.data)
+legend("Tank 1 level","Tank 2 level","Tank 3 level")
+grid on
